e3a4793555e262aa654e1ef6b709a37822e60a2f
Daniel Pavel <daniel.pavel@cgm.com>
2017-08-24 14:41:09 +0300

    mesaj de comentariu

---
+++ GitSample.java
@@ -0,0 +1,16 @@
+package com.cgm.git;
+import java.util.Scanner;
+
+
+public class GitSample {
+
+	public static void main(String[] args) {
+		System.out.println("Please enter your name: ");
+		Scanner text = new Scanner(System.in);
+		String name = text.nextLine();
+		System.out.println("Hello " + name);
+		text.close();
+
+	}
+
+}
